33e9863c7864dcc5ec6a61232e76518ef3276f65
Lamsy <olamide.adetula.1744@gmail.com>
2024-06-29 15:53:51 +0100

    vercel build fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { lineaSepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+  appName: "Mastermind",
+  projectId: "7788ac17c5eb4c5ec6e94a0b48b7a4db",
+  chains: [lineaSepolia],
+  ssr: false, // If your dApp uses server side rendering (SSR)
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <App />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
